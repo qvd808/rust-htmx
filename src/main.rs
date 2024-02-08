6b57569033d9910029ef4672e9df95d3f00299c1
Quang Vinh Dang <dqvinh808@gmail.com>
2024-02-07 22:10:29 -0800

    Run side by side

--- src/main.rs
+++ src/main.rs
@@ -1,71 +1,20 @@
-use axum::{
-    body::Body,
-    http::StatusCode,
-    response::{IntoResponse, Response},
-    routing::{get, post},
-    Json, Router,
-    extract::{Path, Query},
-};
+#![allow(unused)]
 
-use serde::{Serialize, Deserialize};
+use std::net::SocketAddr;
 
-#[derive(Serialize)]
-struct User {
-    id: u64,
-    name: String,
-    email: String,
-}
-
-#[derive( Deserialize)]
-struct Page {
-    number: u32,
-}
-
-//Handle for /create-user
-async fn create_user() -> impl IntoResponse {
-    Response::builder()
-        .status(StatusCode::CREATED)
-        .body(Body::from("User created successfully"))
-        .unwrap()
-}
-
-//Handle  for /users
-async fn list_users() -> Json<Vec<User>> {
-    let users = vec![
-        User {
-            id: 1,
-            name: "Elijah".to_string(),
-            email: "elijah@example.com".to_string(),
-        },
-        User {
-            id: 2,
-            name: "John".to_string(),
-            email: "john@doe.com".to_string()
-        }
-
-    ];
-    Json(users)
-}
-
-async fn show_item(Path(id): Path<u32>, Query(page): Query<Page>) -> String{
-    format!("Item {} on page {}\n", id, page.number)
-}
-
+use axum::{response::Html, Router, routing::get};
 
 #[tokio::main]
 async fn main() {
-    const API: &str = "http://localhost:3000";
+    let route_hello = Router::new().route(
+        "/hello",
+        get(|| async { Html("Hello <strong> World!! </strong>")}),
+    );
 
-    let app = Router::new()
-        .route("/", get(|| async { "Hello, Rust!" }))
-        .route("/create-user", post(create_user))
-        .route("/users", get(list_users))
-        .route("/item/:id", get(show_item));
-
-    println!("Running on {}", API);
-
-    axum::Server::bind(&"127.0.0.1:3000".parse().unwrap())
-        .serve(app.into_make_service())
+    let addr = SocketAddr::from(([127, 0, 0, 1], 8080));
+    println!("Listening on {}", addr);
+    axum::Server::bind(&addr)
+        .serve(route_hello.into_make_service())
         .await
         .unwrap();
 }
