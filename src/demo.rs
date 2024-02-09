5d54cccae8bac9ab7b59a6780a7eab8475c770db
qvd <qvd@asb9820u-f05.csil.sfu.ca>
2024-02-09 13:57:17 -0800

    Adding tailwind and htmx

--- src/demo.rs
+++ src/demo.rs
@@ -1,9 +1,10 @@
-use std::net::SocketAddr;
-use axum::{response::Html, Router, routing::get};
+use axum::{response::Html, routing::get, Router};
+use futures;
 use lazy_static::lazy_static;
+use serde::Serialize;
+use std::net::SocketAddr;
 use tera::Tera;
 use tokio::runtime::Handle;
-use futures;
 
 lazy_static! {
     pub static ref TEMPLATES: Tera = {
@@ -19,27 +20,48 @@ lazy_static! {
     };
 }
 
-async fn root_handler() -> Html<String>{
+#[derive(Serialize)]
+struct Item {
+    name: String,
+    description: String,
+}
 
-    let r = TEMPLATES.render("base.html", &tera::Context::new()).unwrap();
+async fn items_handler() -> Html<String> {
+    let mut context = tera::Context::new();
+    let item_list: [Item; 2] = [
+        Item {
+            name: "Item 1".to_string(),
+            description: "Description of item 1".to_string(),
+        },
+        Item {
+            name: "Item 2".to_string(),
+            description: "Description of item 2".to_string(),
+        },
+    ];
+    context.insert("items", &item_list);
+    let r = TEMPLATES.render("items.html", &context).unwrap();
+    Html(r)
+}
+
+async fn root_handler() -> Html<String> {
+    let r = TEMPLATES
+        .render("base.html", &tera::Context::new())
+        .unwrap();
     Html(r)
 }
 
 #[tokio::main]
-pub async fn demo (handle: Handle) {
-
-    futures::executor::block_on(async  {
-        handle.spawn(
-            async {
-
-            }
-        ).await.expect("Task spawned in Tokio executor panicked!")
+pub async fn demo(handle: Handle) {
+    futures::executor::block_on(async {
+        handle
+            .spawn(async {})
+            .await
+            .expect("Task spawned in Tokio executor panicked!")
     });
-    
-    let route_hello = Router::new().route(
-        "/",
-        get(root_handler),
-    );
+
+    let route_hello = Router::new()
+        .route("/", get(root_handler))
+        .route("/item", get(items_handler));
 
     let addr = SocketAddr::from(([127, 0, 0, 1], 8000));
     println!("Listening on {}", addr);
